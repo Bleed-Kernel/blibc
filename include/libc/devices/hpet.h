67d1ebe081500d634343d3d13538399820a04e5a
mellurboo <mellurblur@gmail.com>
2026-02-27 01:46:55 +0000

    new hpet device

--- include/libc/devices/hpet.h
+++ include/libc/devices/hpet.h
@@ -1,9 +1,7 @@
 #pragma once
 
-#include <stdint.h>
+#define HPET_IOCTL_GET_FEMTOSECONDS 0x4801
 
 #define femtosecondsPerSecond       1000000000000000
 #define femtosecondsPerMillisecond  1000000000000
 #define femtosecondsPerMicrosecond  1000000000
-
-uint64_t _femtoseconds();
--- include/syscalls/femtoseconds.h
+++
@@ -1,6 +0,0 @@
-#ifndef BLIBC_FORWARD_SYSCALLS_FEMTOSECONDS_H
-#define BLIBC_FORWARD_SYSCALLS_FEMTOSECONDS_H
-
-#include <libc/syscalls/femtoseconds.h>
-
-#endif
