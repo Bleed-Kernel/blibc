5b9cdb45b8aa25834289d22d69895c499bb206ca
mellurboo <mellurblur@gmail.com>
2026-03-08 02:46:17 +0000

    matched kernel macros

--- include/libc/devices/console.h
+++ include/libc/devices/console.h
@@ -13,7 +13,7 @@
 #define TTY_IOCTL_GET_WINSIZE       0x5405
 #define TTY_IOCTL_GET_INDEX         0x5406
 
-// linux
+// termios
 #define TTY_IOCTL_TCGETS            0x5407
 #define TTY_IOCTL_TCSETS            0x5408
 #define TTY_IOCTL_TCSETSW           0x5409
@@ -21,8 +21,21 @@
 #define TTY_IOCTL_TIOCGWINSZ        0x5413
 #define TTY_IOCTL_TIOCSWINSZ        0x5414
 #define TTY_IOCTL_FIONBIO           0x5421
+
+// TTY 
 #define TTY_IOCTL_CREATE            0x5422
 #define TTY_IOCTL_SET_ACTIVE        0x5423
+#define TTY_IOCTL_GET_ACTIVE_INDEX  0x5424
+
+// Window 
+#define TTY_IOCTL_WM_CLAIM          0x5425
+#define TTY_IOCTL_WM_RELEASE        0x5426
+#define TTY_IOCTL_WM_GET_MODE       0x5427
+#define TTY_IOCTL_WM_SET_MODE       0x5428
+#define TTY_IOCTL_WM_GET_OWNER      0x5429
+
+#define TTY_WM_MODE_LEGACY          0u
+#define TTY_WM_MODE_EXTERNAL        1u
 
 typedef struct {
     uint32_t x;
