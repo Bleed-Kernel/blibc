11666ab8ab10e580b4e4bcfcc9edeb0318801e59
Myles "Mellurboo" Wilson <102391892+Mellurboo@users.noreply.github.com>
2026-03-14 00:11:24 +0000

    Revert "shared memory interfaces"
    This reverts commit 4ea80d71a4534c19109d165a66efb417becbc0c0.

--- include/libc/syscalls/shm.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <libc/stdint.h>
-#include <libc/stddef.h>
-
-void* _shm_create(size_t pages, uint64_t *out_id);
-void* _shm_map(uint64_t id);
-long _shm_unmap(void *addr);
-long _shm_destroy(uint64_t id);
--- include/syscalls/shm.h
+++
@@ -1,6 +0,0 @@
-#ifndef BLIBC_FORWARD_SYSCALLS_SHM_H
-#define BLIBC_FORWARD_SYSCALLS_SHM_H
-
-#include <libc/syscalls/shm.h>
-
-#endif
