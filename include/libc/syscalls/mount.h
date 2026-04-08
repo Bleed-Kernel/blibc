733a4c6c346634b0d12d1bd462b85002d61814bb
mellurboo <mellurblur@gmail.com>
2026-04-08 20:25:49 +0100

    umount defs

--- include/libc/syscalls/mount.h
+++ include/libc/syscalls/mount.h
@@ -5,3 +5,6 @@ int _mount(const char *source,
            const char *fstype,
            unsigned long flags,
            const void *data);
+
+int _umount(const char *target);
+int _umount2(const char *target, int flags);
