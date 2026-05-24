7c796456d2b16798ec6b13bc4e1f63e5acc71b32
mellurboo <mellurblur@gmail.com>
2026-05-24 03:32:56 +0100

    removed reboot and shutdown

--- include/syscalls/reboot.h
+++
@@ -1,6 +0,0 @@
-#ifndef BLIBC_FORWARD_SYSCALLS_REBOOT_H
-#define BLIBC_FORWARD_SYSCALLS_REBOOT_H
-
-#include <libc/syscalls/reboot.h>
-
-#endif
--- include/syscalls/shutdown.h
+++
@@ -1,6 +0,0 @@
-#ifndef BLIBC_FORWARD_SYSCALLS_SHUTDOWN_H
-#define BLIBC_FORWARD_SYSCALLS_SHUTDOWN_H
-
-#include <libc/syscalls/shutdown.h>
-
-#endif
