b31de2c0e63bbec0e5950c2006d4bc9782705a4f
mellurboo <isabella@chromebook.bleedkernel.com>
2026-07-16 14:29:05 +0100

    included

--- include/libc/stdlib.h
+++ include/libc/stdlib.h
@@ -8,6 +8,7 @@
 int atoi(const char *s);
 double atof(const char *s);
 long strtol(const char *nptr, char **endptr, int base);
+unsigned long strtoul(const char* str, char** endptr, int base)
 void abort(void) __attribute__((noreturn));
 
 void free(void* ptr);
